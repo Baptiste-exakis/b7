ae1c4e5cc899e39326e8191f4f71f1c8e5c22af8
Baptiste-exakis <116570432+Baptiste-exakis@users.noreply.github.com>
2023-02-01 20:34:03 +0100

    Add files via upload

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@ resource "azurerm_ssh_public_key" "ssh_key" {
   name                = "ssh_key_admin"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  public_key          = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCaqpMuafcvfYNbKcGfSa9OGmnhfr3vV0irdGNDlTVV4EBrI0QxkEscOqSZclpbO+DXmx77gSqU/b/vVuM1t9z/mXTKOj7ftm3Au1M0JDXtV82yxb44r0DAwCk7/JsKe5fQpfgV1iCyfMi1qFSR+x1JZZ/mlf61NhYZ8b4SYE+SEeYVl3F90/01bFfA7tzzRtBMKr38SKQcP6HnPTvUzRxBtoV4ZPNRh7c+k9aO2a7A3kxkc36r7gcptji/TIUBGjR2VsNSQ4tULo8WPBBhwSBVjFDEpDe90+J6i2O4STPJBCaYX+MJvLxNyBdFdL4xJBQSqohNmS+Lw3ZerqDJt+SH rsa-key-20230201"
+  public_key          = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCTwIU18gQQbE9Inblg52+IEwSXb84RgFoVbR5Sm3y25JfI/fFdP7ylHeu2tJadVEqvOdK6PaHMeJqNd+adDpHXPnhVYoNhO8qxh/z+CblyZk7Gleztk11YtzCTx62LDCk5O4PI+qOC8sOT7gADt9zJk21jjqITH4dlaWBQNS01ve2mOuOLjmUSe1T4ndK6OXHeObIiC8PNEnnBcMwUqzfnZWTY5WOBibkJx8/vMH4SV00IdGVooPneyn6yD2YgoigGEUMr1Deb1+sAxxXt/xTfuQIRCcZ+AWI0Q1zlF5UwgIPnul03YBcEMygdU/9Y0cWqFW/Nug1S4uCRI2ld5wd9 rsa-key-20230201"
 }
 
 resource "random_string" "password" {
